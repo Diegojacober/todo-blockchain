3a08d85a27a86ceb952243c9106f7bd9ec21a2b5
Diego <diegoalencar.jacober@gmail.com>
2024-05-23 21:09:45 -0300

    :construction:

--- src/app.js
+++ src/app.js
@@ -1,29 +1,107 @@
 App = {
-    load: async () => {
-        await App.loadWeb3()
-    },
+  contracts: {},
+  loading: false,
 
-    loadWeb3: async () => {
-        if (window.ethereum) {
-            const web3 = new Web3(window.ethereum);
-            // Request the user to connect accounts (Metamask will prompt)
-            await window.ethereum.request({ method: 'eth_requestAccounts' });
-        
-            // Get the connected accounts
-            const accounts = await web3.eth.getAccounts();
-        
-            // Display the connected account
-            document.getElementById('connectedAccount').innerText = accounts[0];
-          } else {
-            // Alert the user to download Metamask
-            alert('Please download Metamask');
-          }
-    },
+  load: async () => {
+    await App.loadWeb3();
+    await App.loadAccount();
+    await App.loadContract();
+    await App.render();
+  },
 
-}
+  loadWeb3: async () => {
+    if (window.ethereum) {
+      const web3 = new Web3(window.ethereum);
+      // Request the user to connect accounts (Metamask will prompt)
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+
+      App.web3 = web3;
+    } else {
+      // Alert the user to download Metamask
+      alert("Please download Metamask");
+    }
+  },
+
+  loadAccount: async () => {
+    const accounts = await App.web3.eth.getAccounts();
+    App.account = accounts[0];
+  },
+
+  loadContract: async () => {
+    const todoList = await $.getJSON("TodoList.json");
+    App.contracts.TodoList = TruffleContract(todoList);
+    App.contracts.TodoList.setProvider(window.ethereum);
+
+    // Hydrate the smart contract with the values form the blockchain
+    App.todoList = await App.contracts.TodoList.deployed();
+  },
+
+  render: async () => {
+    // prevent double render
+    if (App.loading) {
+      return;
+    }
+
+    // update app loading state
+    App.setLoading(true);
+
+    $("#account").html(App.account);
+
+    await App.renderTasks();
+
+    App.setLoading(false);
+  },
+
+  renderTasks: async () => {
+    // Load the total task count from blockchain
+    const taskCount = await App.todoList.taskCount();
+    const $taskTemplate = $(".taskTemplate");
+
+    // Render out each task with a new task template
+    for (var i = 1; i <= taskCount; i++) {
+      // Fetch the task data from the blockchain
+      const task = await App.todoList.tasks(i);
+      const taskId = task[0].toNumber();
+      const taskContent = task[1];
+      const taskCompleted = task[2];
+
+      // Create the html for the task
+      const $newTaskTemplate = $taskTemplate.clone();
+      $newTaskTemplate.find(".content").html(taskContent);
+      $newTaskTemplate
+        .find("input")
+        .prop("name", taskId)
+        .prop("checked", taskCompleted)
+        .on("click", App.toggleCompleted);
+
+      // Put the task in the correct list
+      if (taskCompleted) {
+        $("#completedTaskList").append($newTaskTemplate);
+      } else {
+        $("#taskList").append($newTaskTemplate);
+      }
+
+      // Show the task
+      $newTaskTemplate.show();
+    }
+  },
+
+  setLoading: (boolean) => {
+    App.loading = boolean;
+    const loader = $("#loader");
+    const content = $("#content");
+    if (boolean) {
+      loader.show();
+      content.hide();
+    } else {
+      loader.hide();
+      content.show();
+    }
+  },
+};
 
 $(() => {
-    $(window).load(() => {
-        App.load()
-    })
-})
+  $(window).load(() => {
+    App.load();
+  });
+});
