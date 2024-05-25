7c6aaae23287449aebf0f48585a6401aa652be99
Diego <diegoalencar.jacober@gmail.com>
2024-05-25 11:45:42 -0300

    :construction: create tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -14,10 +14,18 @@ contract TodoList {
         bool completed;
     }
 
+    // like a Map<> in java
     mapping(uint => Task) public tasks;
+
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
 
     function createTask(string memory _content) public {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
     }
 }
