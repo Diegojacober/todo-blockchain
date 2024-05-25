b4398990ea272b0121481c9e2b340632bc13b3e6
Diego <diegoalencar.jacober@gmail.com>
2024-05-25 12:00:01 -0300

    feat: complet tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -17,9 +17,10 @@ contract TodoList {
     // like a Map<> in java
     mapping(uint => Task) public tasks;
 
-    event TaskCreated(
+    event TaskCreated(uint id, string content, bool completed);
+
+    event TaskCompleted(
         uint id,
-        string content,
         bool completed
     );
 
@@ -27,5 +28,12 @@ contract TodoList {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
         emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
     }
 }
